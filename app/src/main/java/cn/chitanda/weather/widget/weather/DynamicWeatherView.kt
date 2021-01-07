8f671017d5aa774ff2bc3c93848daf59bca936fa
chen <chunjinchen1998@gmail.com>
2021-01-07 09:20:51 +0800

    使用SurfacView实现天气动画

--- app/src/main/java/cn/chitanda/weather/widget/weather/DynamicWeatherView.kt
+++ app/src/main/java/cn/chitanda/weather/widget/weather/DynamicWeatherView.kt
@@ -3,44 +3,86 @@ package cn.chitanda.weather.widget.weather
 import android.content.Context
 import android.graphics.Canvas
 import android.util.AttributeSet
-import android.view.View
+import android.util.Log
+import android.view.SurfaceHolder
+import android.view.SurfaceView
 import cn.chitanda.weather.widget.weather.controller.IController
+import kotlin.concurrent.thread
 
 /**
  * @Author:       Chen
- * @Date:         2020/12/30 10:07
+ * @Date:         2021/1/6 15:12
  * @Email:        "chunjinchen1998@gmail.com"
  * @Description:
  */
-private const val TAG = "DynamicWeatherView"
+class DynamicWeatherView2 @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : SurfaceView(context, attrs, defStyleAttr), SurfaceHolder.Callback {
 
-class DynamicWeatherView @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr) {
-    var weatherController: IController? = null
-    private var isMeasured = false
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        val viewWidth = resolveSize(suggestedMinimumWidth, widthMeasureSpec)
-        val viewHeight = resolveSize(suggestedMinimumHeight, heightMeasureSpec)
-        setMeasuredDimension(viewWidth, viewHeight)
-        weatherController?.init(this, viewWidth, viewHeight)
-        isMeasured = true
-    }
+    private var _controller: IController? = null
+    var weatherController: IController?
+        get() = _controller
+        set(value) {
+            _controller = value
+            if (isCreated) {
+                init()
+            } else {
+                postDelayed({ init() }, 1000)
+            }
+        }
+    private var isRunning = false
+    private lateinit var canvas: Canvas
+    private lateinit var drawThread: Thread
+    private var isCreated = false
+    private val refreshRate: Long
+        get() = 1000L / (context.display?.refreshRate ?: 60f).toInt()
 
-    override fun onDraw(canvas: Canvas?) {
-//        super.onDraw(canvas)
-        canvas?.let {
-            weatherController?.draw(it)
+    private fun init() {
+        if (isRunning) drawThread.interrupt()
+        weatherController?.init(this, width, height)
+        isRunning = true
+        drawThread = thread {
+            while (isRunning) {
+                try {
+                    synchronized(holder) {
+                        canvas = holder.lockCanvas()
+                        weatherController?.draw(canvas)
+                        holder.unlockCanvasAndPost(canvas)
+                    }
+                    Thread.sleep(refreshRate)
+                } catch (e: Throwable) {
+                    e.printStackTrace()
+                }
+            }
         }
     }
 
-    override fun postInvalidate() {
-        if (!isMeasured) return
-        super.postInvalidate()
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        Log.d(TAG, "surfaceCreated: ")
     }
 
-    enum class Type {
-        Sunny, Cloudy, Rain, Snow,
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+        Log.d(TAG, "surfaceChanged: ")
+        isCreated = true
+        weatherController?.let {
+            init()
+        }
     }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        Log.d(TAG, "surfaceDestroyed: ")
+        isRunning = false
+        drawThread.interrupt()
+    }
+
+    init {
+        holder.addCallback(this)
+        Log.d(TAG, "init: $holder")
+    }
+
 }
+
+private const val TAG = "DynamicWeatherView2"
